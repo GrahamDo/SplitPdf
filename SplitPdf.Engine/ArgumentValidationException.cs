bd447d7af9e4e9a91279c67969234661925f1847
GrahamDo <graham@communicate.za.net>
2019-03-01 15:36:44 +0200

    Issue #1 (#2)
    * #1 Create structure for ArgumentsInterpreter and tests.
    
    * #1 Testing that passing null or an empty array to ProcessArguments throws an exception with a usage message.
    
    * #1 Testing that passing one or more files to ProcessArguments populates the InputFiles list
    
    * #1 Add clarification in README, that you need .NET 4.6.1 to run the unit tests.
    
    * #1 Remove ability for ArgumentsInterpreter to check for missing files. That's really the responsibility of the Runner.
    
    * #1 Testing that if -m is the only argument passed, you get an exception because there's nothing to merge.
    
    * #1 Test merge criteria:
    
    * If merge is specified, it must be the first argument
    * If merge is specified, there must be at least three other arguments (two input files, and an output file)
    * If merge is specified, the output file cannot be the same as any of the input files
    
    * #1 One last NON-merge test: You can't pass duplicate file names to the arguments.
    
    * Update NuGet packages for test framework.
    
    * #1 Change program to rather use the ArgumentsInterpreter class instead of trying to interpret the args itself.

---
+++ SplitPdf.Engine/ArgumentValidationException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace SplitPdf.Engine
+{
+  public class ArgumentValidationException : ApplicationException
+  {
+    public ArgumentValidationException(string message) : base(message) { }
+
+    public static void ThrowWithUsageMessage()
+      => ThrowWithUsageMessage(null);
+
+    public static void ThrowWithUsageMessage(string message)
+    {
+      var finalMessage = new StringBuilder(2);
+      if (message != null)
+      {
+        finalMessage.AppendLine(message);
+        finalMessage.AppendLine();
+      }
+
+      finalMessage.Append(ArgumentsInterpreter.UsageMessage);
+      throw new ArgumentValidationException(finalMessage.ToString());
+    }
+  }
+}
