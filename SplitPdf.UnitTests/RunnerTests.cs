5d7947d55c09f0adcb187499b77940a23b49f4a0
Graham Downs <>
2019-03-04 13:04:25 +0200

    . #3 Create test Run_PassPdfFile1AndPdfFile2_Should_Create8Files

--- SplitPdf.UnitTests/RunnerTests.cs
+++ SplitPdf.UnitTests/RunnerTests.cs
@@ -27,5 +27,33 @@ namespace SplitPdf.UnitTests
         File.Delete(fileName);
       }
     }
+
+    [TestMethod]
+    public void Run_PassPdfFile1AndPdfFile2_Should_Create8Files()
+    {
+      var progressMessages = new List<string>();
+
+      var inputFiles = new List<string> { "PDF-File1.pdf", "PDF-File2.pdf" };
+      var runner = new Runner(new ArgumentsValidator());
+      runner.Progress += (sender, e) => { progressMessages.Add(e.ProgressMessage); };
+      runner.Run(inputFiles);
+
+      Assert.AreEqual(8, progressMessages.Count);
+      for (var i = 0; i < 4; i++)
+      {
+        var fileName = $"PDF-File1-Page{i + 1}of4.pdf";
+        Assert.AreEqual($"Creating file: {fileName}", progressMessages[i]);
+        Assert.IsTrue(File.Exists(fileName), $"{fileName} does not exist.");
+        File.Delete(fileName);
+      }
+
+      for (var i = 0; i < 4; i++)
+      {
+        var fileName = $"PDF-File2-Page{i + 1}of4.pdf";
+        Assert.AreEqual($"Creating file: {fileName}", progressMessages[i + 4]);
+        Assert.IsTrue(File.Exists(fileName), $"{fileName} does not exist.");
+        File.Delete(fileName);
+      }
+    }
   }
 }
