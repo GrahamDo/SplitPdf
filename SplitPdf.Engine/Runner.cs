f317bb468a6bb876e25d88405c803678c6722b99
GrahamDo <graham@communicate.za.net>
2019-03-04 13:38:33 +0200

    Issue #3: Change program to use the Runner to actually do the work. (#4)
    * #3 Move logic to check that there are at least two input files with merge from ArgumentInterpeter to ArgumentValidator.
    
    * #3 Move PassDuplicateFileNamesWithoutMerge_Should_ThrowException logic from Interpreter to validator.
    
    * #3 Create test Validate_PassZeroInputFiles_Should_ThrowException
    
    * #3 Create test Validate_PassNullInputFiles_Should_ThrowException
    
    * #3 Move PassingMergeWithSameOutputFileAsInputFile_Should_ThrowException logic from interpreter to validator.
    
    * #3 Create test Validate_PassingNonExistentInputFile_Should_ThrowException
    
    * #3 Create test Validate_PassingExistentMergeOutputFile_Should_ThrowException
    
    * . #3 Create test Run_PassPdfFile1_Should_Create4Files
    
    * . #3 Create test Run_PassPdfFile1AndPdfFile2_Should_Create8Files
    
    * . #3 Create test Run_MergePdfFile1AndPdfFile2AndPdfFile3_Should_CreateOutputFile
    
    * . #3 Change program to use the Runner to actually do the work.

---
+++ SplitPdf.Engine/Runner.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+
+namespace SplitPdf.Engine
+{
+  public class Runner
+  {
+    private readonly ArgumentsValidator _argumentsValidator;
+
+    public Runner(ArgumentsValidator argumentsValidator)
+    {
+      _argumentsValidator = argumentsValidator;
+    }
+
+    public delegate void RunnerProgressHandler(object sender, RunnerProgressEventArgs e);
+
+    public event RunnerProgressHandler Progress;
+
+    public void Run(List<string> inputFiles) => Run(inputFiles, null);
+    public void Run(List<string> inputFiles, string mergeOutputFile)
+    {
+      _argumentsValidator.Validate(inputFiles, mergeOutputFile);
+
+      if (string.IsNullOrEmpty(mergeOutputFile))
+        foreach (var file in inputFiles)
+          DoSplit(file);
+      else
+        DoMerge(inputFiles, mergeOutputFile);
+    }
+
+    private void DoSplit(string file)
+    {
+      var inputDocument = PdfReader.Open(file, PdfDocumentOpenMode.Import);
+      var destFolder = Path.GetDirectoryName(inputDocument.FullPath);
+      var destFileName = Path.GetFileNameWithoutExtension(file);
+      var destFileExtension = Path.GetExtension(file);
+      for (var i = 0; i < inputDocument.PageCount; i++)
+      {
+        var destFileNameFinal = $"{destFileName}-Page{i + 1}of{inputDocument.PageCount}{destFileExtension}";
+        Progress?.Invoke(this, new RunnerProgressEventArgs
+        {
+          ProgressMessage = $"Creating file: {destFileNameFinal}"
+        });
+        var outputDocument = new PdfDocument { Version = inputDocument.Version };
+        outputDocument.AddPage(inputDocument.Pages[i]);
+        outputDocument.Save($"{destFolder}\\{destFileNameFinal}");
+      }
+    }
+
+    private void DoMerge(List<string> inputFiles, string mergeOutputFile)
+    {
+      var outputDocument = new PdfDocument();
+      inputFiles.ForEach(file =>
+      {
+        Progress?.Invoke(this, new RunnerProgressEventArgs
+        {
+          ProgressMessage = $"Processing {file}"
+        });
+        var inputDocument = PdfReader.Open(file, PdfDocumentOpenMode.Import);
+        var count = inputDocument.PageCount;
+        for (var idx = 0; idx < count; idx++)
+        {
+          var page = inputDocument.Pages[idx];
+          outputDocument.AddPage(page);
+        }
+      });
+      Progress?.Invoke(this, new RunnerProgressEventArgs
+      {
+        ProgressMessage = $"Creating output file {mergeOutputFile}"
+      });
+      outputDocument.Save(mergeOutputFile);
+
+    }
+  }
+}
---
+++ SplitPdf.UnitTests/RunnerTests.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SplitPdf.Engine;
+
+namespace SplitPdf.UnitTests
+{
+  [TestClass]
+  public class RunnerTests
+  {
+    [TestMethod]
+    public void Run_PassPdfFile1_Should_Create4Files()
+    {
+      var progressMessages = new List<string>();
+
+      var inputFiles = new List<string> {"PDF-File1.pdf"};
+      var runner = new Runner(new ArgumentsValidator());
+      runner.Progress += (sender, e) => progressMessages.Add(e.ProgressMessage);
+      runner.Run(inputFiles);
+
+      Assert.AreEqual(4, progressMessages.Count);
+      for (var i = 0; i < 4; i++)
+      {
+        var fileName = $"PDF-File1-Page{i + 1}of4.pdf";
+        Assert.AreEqual($"Creating file: {fileName}", progressMessages[i]);
+        Assert.IsTrue(File.Exists(fileName), $"{fileName} does not exist.");
+        File.Delete(fileName);
+      }
+    }
+
+    [TestMethod]
+    public void Run_PassPdfFile1AndPdfFile2_Should_Create8Files()
+    {
+      var progressMessages = new List<string>();
+
+      var inputFiles = new List<string> { "PDF-File1.pdf", "PDF-File2.pdf" };
+      var runner = new Runner(new ArgumentsValidator());
+      runner.Progress += (sender, e) => progressMessages.Add(e.ProgressMessage);
+      runner.Run(inputFiles);
+
+      Assert.AreEqual(8, progressMessages.Count);
+      for (var i = 0; i < 4; i++)
+      {
+        var fileName = $"PDF-File1-Page{i + 1}of4.pdf";
+        Assert.AreEqual($"Creating file: {fileName}", progressMessages[i]);
+        Assert.IsTrue(File.Exists(fileName), $"{fileName} does not exist.");
+        File.Delete(fileName);
+      }
+
+      for (var i = 0; i < 4; i++)
+      {
+        var fileName = $"PDF-File2-Page{i + 1}of4.pdf";
+        Assert.AreEqual($"Creating file: {fileName}", progressMessages[i + 4]);
+        Assert.IsTrue(File.Exists(fileName), $"{fileName} does not exist.");
+        File.Delete(fileName);
+      }
+    }
+
+    [TestMethod]
+    public void Run_MergePdfFile1AndPdfFile2AndPdfFile3_Should_CreateOutputFile()
+    {
+      var progressMessages = new List<string>();
+
+      var inputFiles = new List<string> { "PDF-File1.pdf", "PDF-File2.pdf", "PDF-File3.pdf" };
+      var outputFile = "output.pdf";
+      var runner = new Runner(new ArgumentsValidator());
+      runner.Progress += (sender, e) => progressMessages.Add(e.ProgressMessage);
+      runner.Run(inputFiles, outputFile);
+
+      Assert.AreEqual(4, progressMessages.Count);
+      for (var i = 0; i < 3; i++)
+        Assert.AreEqual($"Processing {inputFiles[i]}", progressMessages[i]);
+      Assert.AreEqual($"Creating output file {outputFile}", progressMessages[3]);
+      Assert.IsTrue(File.Exists(outputFile), "Failed to create output file");
+      File.Delete(outputFile);
+    }
+  }
+}
