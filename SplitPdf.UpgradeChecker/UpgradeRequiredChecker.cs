8e63a0faa235578823b9b4fcad260f2522d59607
Graham Downs <>
2019-03-06 15:48:54 +0200

    . #5 Create methods for UpgradeRequiredChecker to get and save the date the application last checked for upgrades (Can't think of a way to unit test those methods, though)

--- SplitPdf.UpgradeChecker/UpgradeRequiredChecker.cs
+++ SplitPdf.UpgradeChecker/UpgradeRequiredChecker.cs
@@ -1,26 +1,62 @@
 ﻿using System;
+using System.IO;
 using System.Net;
 
 namespace SplitPdf.UpgradeChecker
 {
-    public class UpgradeRequiredChecker
+  public class UpgradeRequiredChecker
+  {
+    public DateTime GetLastChecked()
     {
-      public LatestVersionInfo GetLatestVersionInfoFromUrl(string url)
+      var path = Path.Combine(Environment.GetFolderPath(
+        Environment.SpecialFolder.ApplicationData), "GrahamDo");
+      if (!Directory.Exists(path))
+        return DateTime.MinValue;
+
+      path = Path.Combine(path, "SplitPdf");
+      if (!Directory.Exists(path))
+        return DateTime.MinValue;
+
+      var fileName = $"{path}{Path.DirectorySeparatorChar}" +
+                     "LastUpdateCheck";
+
+      return !File.Exists(fileName) ? DateTime.MinValue : DateTime.Parse(File.ReadAllText(fileName));
+    }
+
+    private static void SetDateLastChecked()
+    {
+      var path = Path.Combine(Environment.GetFolderPath(
+        Environment.SpecialFolder.ApplicationData), "GrahamDo");
+      if (!Directory.Exists(path))
+        Directory.CreateDirectory(path);
+
+      path = Path.Combine(path, "SplitPdf");
+      if (!Directory.Exists(path))
+        Directory.CreateDirectory(path);
+
+      var fileName = $"{path}{Path.DirectorySeparatorChar}" +
+                     "LastUpdateCheck";
+      File.WriteAllText(fileName, 
+        DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+    }
+
+    public LatestVersionInfo GetLatestVersionInfoFromUrl(string url)
+    {
+      using (var client = new WebClient())
       {
-        using (var client = new WebClient())
-        {
-          var rawString = client.DownloadString(url);
+        var rawString = client.DownloadString(url);
 
-          var elements = rawString.Split('|');
-          var versionElements = elements[0].Split('.');
-          var major = Convert.ToUInt32(versionElements[0]);
-          var minor = Convert.ToUInt32(versionElements[1]);
-          var build = Convert.ToUInt32(versionElements[2]);
-          var revision = Convert.ToUInt32(versionElements[3]);
+        var elements = rawString.Split('|');
+        var versionElements = elements[0].Split('.');
+        var major = Convert.ToUInt32(versionElements[0]);
+        var minor = Convert.ToUInt32(versionElements[1]);
+        var build = Convert.ToUInt32(versionElements[2]);
+        var revision = Convert.ToUInt32(versionElements[3]);
 
-          return new LatestVersionInfo(major, minor, build, revision, 
-            elements[1]);
-        }
+        SetDateLastChecked();
+        return new LatestVersionInfo(major, minor, build, revision,
+          elements[1]);
+      }
     }
-    }
+  }
 }
