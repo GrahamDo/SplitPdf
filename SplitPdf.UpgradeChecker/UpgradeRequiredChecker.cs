f60371a19ec1e92b8c3cd439e8d545c4b5f7a0a1
GrahamDo <graham@communicate.za.net>
2019-03-06 17:42:06 +0200

    Issue 5 - Implemented version checking, both on-demand and once every 14 days (#6)
    * . #5 Create test GetLatestVersionInfo_Should_ReturnValidVersionInfo
    
    * . #5 Create methods for UpgradeRequiredChecker to get and save the date the application last checked for upgrades (Can't think of a way to unit test those methods, though)
    
    * . #5 Create test IsUpgradeRequired_Should_ReturnTrueForVersion1dot0dot0dot0
    
    * . #5 Add a bunch more scenarios to make sure we've got the IsUpgradeRequired check working 100%
    
    * . #5 Create test ProcessArguments_PassingUCWithAnythingElse_Should_ThrowException
    
    * . #5 Create test ProcessArguments_PassOnlyUC_Should_SetIsUpgradeCheckRequested
    
    * . #5 Create test ProcessArguments_PassOnlyUC_Should_StopInterpretingArguments
    
    * . #5 Finalise upgrade checking.

---
+++ SplitPdf.UpgradeChecker/UpgradeRequiredChecker.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace SplitPdf.UpgradeChecker
+{
+  public class UpgradeRequiredChecker
+  {
+    public DateTime GetLastChecked()
+    {
+      var path = Path.Combine(Environment.GetFolderPath(
+        Environment.SpecialFolder.ApplicationData), "GrahamDo");
+      if (!Directory.Exists(path))
+        return DateTime.MinValue;
+
+      path = Path.Combine(path, "SplitPdf");
+      if (!Directory.Exists(path))
+        return DateTime.MinValue;
+
+      var fileName = $"{path}{Path.DirectorySeparatorChar}" +
+                     "LastUpdateCheck";
+
+      return !File.Exists(fileName) ? DateTime.MinValue : DateTime.Parse(File.ReadAllText(fileName));
+    }
+
+    public void SetDateLastChecked()
+    {
+      var path = Path.Combine(Environment.GetFolderPath(
+        Environment.SpecialFolder.ApplicationData), "GrahamDo");
+      if (!Directory.Exists(path))
+        Directory.CreateDirectory(path);
+
+      path = Path.Combine(path, "SplitPdf");
+      if (!Directory.Exists(path))
+        Directory.CreateDirectory(path);
+
+      var fileName = $"{path}{Path.DirectorySeparatorChar}" +
+                     "LastUpdateCheck";
+      File.WriteAllText(fileName, 
+        DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+    }
+
+    public LatestVersionInfo GetLatestVersionInfoFromUrl(string url)
+    {
+      using (var client = new WebClient())
+      {
+        var rawString = client.DownloadString(url);
+
+        var elements = rawString.Split('|');
+        var versionElements = elements[0].Split('.');
+        var major = Convert.ToInt32(versionElements[0]);
+        var minor = Convert.ToInt32(versionElements[1]);
+        var build = Convert.ToInt32(versionElements[2]);
+        var revision = Convert.ToInt32(versionElements[3]);
+
+        return new LatestVersionInfo(major, minor, build, revision,
+          elements[1]);
+      }
+    }
+
+    public bool IsUpgradeRequired(int actualMajor, int actualMinor, 
+      int actualBuild, int actualRevision, 
+      LatestVersionInfo versionInfo)
+    {
+      if (versionInfo.VersionMajor < actualMajor)
+        return false;
+      if (versionInfo.VersionMajor > actualMajor)
+        return true;
+      if (versionInfo.VersionMajor != actualMajor)
+        return false;
+
+      if (versionInfo.VersionMinor < actualMinor)
+        return false;
+      if (versionInfo.VersionMinor > actualMinor)
+        return true;
+      if (versionInfo.VersionMinor != actualMinor)
+        return false;
+
+      if (versionInfo.VersionBuild < actualBuild)
+        return false;
+      if (versionInfo.VersionBuild > actualBuild)
+        return true;
+      if (versionInfo.VersionBuild == actualBuild)
+        return versionInfo.VersionRevision > actualRevision;
+
+      return false;
+    }
+  }
+}
