0c356a3f813962c89f2579b98fc98d8a807e76fc
Graham Downs <graham@communicate.za.net>
2021-03-02 16:45:39 +0200

    Increase copyright year, version to 2.6.8.0

--- SplitPdf.UpgradeChecker/Properties/AssemblyInfo.cs
+++ SplitPdf.UpgradeChecker/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SplitPdf.UpgradeChecker")]
-[assembly: AssemblyCopyright("Copyright © Graham Downs 2019")]
+[assembly: AssemblyCopyright("Copyright © Graham Downs 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.5.7.1")]
+[assembly: AssemblyVersion("2.6.8.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
